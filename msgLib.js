5d353f7bab41d90cd506a41841c50ac7c0c10738
Richard791-rucdary <badgcheets@gmail.com>
2026-09-15 13:21:43 +0100

    Refactor AlertBox to DisBox with updated parameters

--- msgLib.js
+++ msgLib.js
@@ -8,20 +8,27 @@ const msgLib = (() => {
     }
     let resolver = null
     let alertLength = [];
-function AlertBox(val) {
-    if(!animations[val.animation]) val.animation = "slide"
-    if(isNaN(val.width, val.height)) throw new Error(`Width and height of AlertBox must be a number.`)
+function DisBox(val) {
+    let width = !isNaN(val.width) ? val.width: 200;
+    let height = !isNaN(val.height) ? val.height: 200;
+    let backdrop = val.background? val.background: "white";
+    let xPos = !isNaN(val.x) ? val.x : 10;
+    let yPos =!isNaN(val.y) ? val.y:  30;
+    let btn_back = val.btn_background ? val.btn_background: "black"
+    let btn_width = val.btn_width ? val.btn_width: width*20/100;
+let btn_col = val.btn_color?val.btn_color: "white"
+let animate = animations[val.animation] ? val.animation: "slide"
     let stylesheet = `
 <style>
 #alert {
- width: ${val.width || 200}px;
- height: ${val.height || 200}px;
-background: ${val.background};
+ width: ${width}px;
+ height: ${height}px;
+background: ${backdrop};
 border-radius: 8px;
 border: none;
 position: relative;
-top: ${val.y || 30}%;
-left:${val.x || 10}%;
+top: ${yPos}%;
+left:${xPos}%;
 z-index:1000;
 text-align: center;
 }
@@ -46,34 +53,34 @@ opacity: 0;
 
 @keyframes jump {
 0% {
-top: ${val.y || 30}%;
+top: ${yPos}%;
 }
 30% {
-top: ${val.y-10 || 20}%;
+top: ${yPos-10}%;
 }
 50% {
-top: ${val.y || 30}%;
+top: ${yPos}%;
 }
 100% {
 top: -100%;
 }
 }
 
-}
 
 @keyframes slide {
 0% {
 width: 0px;
 }
 100% {
-width: ${val.width || 200}px;
+width: ${width}px;
 }
 }
+
 #alert button {
-background: ${val.btn_background || "black"};
- min-width: ${val.btn_width || val.width*20/100}px;
+background: ${btn_back};
+ min-width: ${btn_width}px;
 min-height: 20px;
-color: ${val.btn_color || "white"};
+color: ${btn_col};
 border-radius: 8px;
 border: none;
 word-wrap: break-word;
@@ -83,7 +90,7 @@ word-break: break-word;
 
 @keyframes unslide {
 0% {
-width: ${val.width || 200}px;
+width: ${width}px;
 }
 100% {
 width: 0px;
@@ -93,12 +100,12 @@ width: 0px;
 
 @keyframes up_slide {
 0% {
-width: ${val.width || 200}px;
-height: ${val.height || 200}px;
+width: ${width}px;
+height: ${height}px;
 }
 50% {
 width: 5px;
-height: ${val.height || 200}px;
+height: ${height}px;
 }
 100% {
 width:5px;
@@ -113,11 +120,11 @@ height: 5px;
 }
 50% {
 width: 5px;
-height: ${val.height || 200}px;
+height: ${height}px;
 }
 100% {
-width:${val.width || 200}px;
-height: ${val.height || 200}px;
+width:${width}px;
+height: ${height}px;
 }
 }
 
@@ -125,26 +132,30 @@ height: ${val.height || 200}px;
     width: 100%;
     height: 100vh;
     background:  hsla(0, 8%, 5%, 0.8);
+     position: fixed;
      z-index: 1000;
+     top: 0%;
      }
+
      .asend {
      position: absolute;
      top: 80%;
      left: ${val.displayCancelButton?50:72}%;
+     display: ${val.displayNoButtons? "none": "block"}
      }
      .asif {
        position: absolute;
      top: 80%;
      left: 72%;
-     display: ${val.displayCancelButton ? "block" :"none"};
+     display: ${val.displayNoButtons? "none":val.displayCancelButton ? "block" :"none"};
      }
 
      .slides {
-animation: ${val.animation || "slide"} 1s;
+animation: ${animate} 1s;
      }
 
 .unani {
- animation: ${animations[val.animation].close || "unslide"} 1s;
+ animation: ${animations[val.animation]?animations[val.animation].close : "unslide"} 1s;
 }
 
 
@@ -153,24 +164,16 @@ animation: ${val.animation || "slide"} 1s;
   word-wrap: break-word;
 overflow-wrap: break-word;
 word-break: break-word;
- background: ${val.background || "white"};
+ background: ${backdrop};
     border-radius: 8px;
-     width:${val.width || 200}px;
-    height: ${val.height || 200}px;
+     width:${width}px;
+    height: ${height}px;
     position: relative;
-top: ${val.y || 30}%;
-left:${val.x +30 || 50}%;
+top: ${yPos}%;
+left:${xPos}%;
     text-align: center;
     }
     
-@keyframes slide {
-0% {
-width: 0px;
-}
-100% {
-width: ${val.width || 200}px;
-}
-}
 }
 
 
@@ -179,13 +182,13 @@ width: ${val.width || 200}px;
 top: -100%;
 }
 50% {
-top: ${val.y || 30}%;
+top: ${yPos}%;
 }
 70% {
-top: ${val.y-10 || 20}%
+top: ${yPos-10}%
 }
 100% {
-top: ${val.y || 30}%
+top: ${yPos}%;
 }
 </style>
 `
@@ -198,7 +201,7 @@ document.body.appendChild(dis)
 document.getElementById("alert").classList.add("slides")
 setTimeout(() => {
     document.getElementById("alert").classList.remove("slides")
-    document.getElementById("alert").innerHTML = `<br><h1 style="color: ${val.title_color ? val.title_color: val.btn_color? val.btn_color:  "black"};">${val.title_content || ""}</h1><div style="color: ${val.text_color || 'black'};">${val.content}</div><button class="asend" onclick=${val.displayCancelButton ? "msgLib.abnClose()" : "msgLib.yes()"}>Ok</button><button class="asif" onclick="msgLib.no()">Cancel</button>`
+    document.getElementById("alert").innerHTML = `<br><h1 style="color: ${val.title_color ? val.title_color: val.btn_color? val.btn_color:  "black"};">${val.title_content ? val.title_content:""}</h1><div style="color: ${val.text_color?val.text_color:'black'};">${val.content}</div><button class="asend" onclick=${val.displayCancelButton ? "msgLib.abnClose()" : "msgLib.closeTrue()"}>Ok</button><button class="asif" onclick="msgLib.closeFalse()">Cancel</button>`
 }, 1000)
 
 
@@ -223,7 +226,7 @@ function close(bool) {
  document.querySelector(".backdrop").innerHTML = "";
   document.body.removeChild(dis)
   if(alertLength.length > 0) {
-    AlertBox(alertLength[0]);
+    DisBox(alertLength[0]);
     alertLength.splice(0,1)
   }
     }, 900)
@@ -231,21 +234,22 @@ function close(bool) {
 
 function abnClose() {
       let obj = {};
-    document.querySelectorAll("#alert input").forEach(val => {
+    document.querySelectorAll("#alert input").forEach((val,idx) => {
         if(!val) return  close(true)
         if(val.id) {
             obj[val.id] = val.value
         } else {
-            obj["input_"+Object.entries(obj).length] = val.value
+            obj["input_"+idx+1] = val.value
         }
     })
     close(obj)
     
 }
+
 return {
-    AlertBox,
-    yes : (() => close(true)),
-    no: (() => close(false)),
+    DisBox,
+    closeTrue : (() => abnClose()),
+    closeFalse: (() => close(false)),
     abnClose,
 }
 }) ()
